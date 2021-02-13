0740c6abc549c06583ce031d2c468c9baf368eb2
ShotsFired375 <68354767+ShotsFired375@users.noreply.github.com>
2021-02-13 12:52:28 -0600

    Add files via upload

---
+++ js/Hero.js
@@ -0,0 +1,25 @@
+class Hero {
+  constructor (x,y,r) {
+    var options = {
+      density: 1,
+      frictionAir: 1
+    }
+    this.x=x;
+    this.y=y;
+    this.r=r;
+    superHeroImg = loadImage("images/Superhero-01.png");
+    this.body = Bodies.circle(this.x, this.y, (this.r), options);
+    World.add(world, this.body);
+  }
+  display() {
+      var pos = this.body.position;
+      var angle = this.body.angle;
+      
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER);
+      image(superHeroImg, 0, 0, this.r+100, this.r-50);
+      pop();
+  }
+}
